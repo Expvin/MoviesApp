18df9d66e0fbc5d50c28717bc488c7a7b256b65b
Oleg <olegmanu9@gmail.com>
2023-03-20 16:55:36 +0300

    item layout create

---
+++ app/src/main/java/com/expv1n/myfilmsapp/presentation/adapter/MovieAdapter.kt
@@ -0,0 +1,26 @@
+package com.expv1n.myfilmsapp.presentation.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class MovieAdapter: RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
+
+    class MovieViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
